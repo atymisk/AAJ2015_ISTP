c30ab02adbe33a2e2cd96dba5fa7e28bbb8be05f
leecn1 <leecn1@uci.edu>
2015-04-21 07:34:16 -0700

    added RateBehavior scene

---
+++ Assets/Scripts/Submit_Button.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Submit_Button : MonoBehaviour {
+	GameObject explain;
+	// Use this for initialization
+	void Start () {
+		explain = GameObject.Find ("ExplainBehavior");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void clickNow(){
+		string text = explain.GetComponentInChildren<InputField> ().text;
+		print (text);
+	}
+}
